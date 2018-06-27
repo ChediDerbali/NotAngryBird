617127ad26a4cc237e91732f68684b8bb0817a87
ChediDerbali <chederbali@gmail.com>
2018-06-27 14:59:33 +0100

    first commit

---
+++ Assets/Scripts/Enemy.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Enemy : MonoBehaviour
+{
+
+    public GameObject deathEffect;
+
+    public float health = 4f;
+    public Text result;
+    public GameObject menu;
+    public static int EnemiesAlive = 0;
+
+
+    void Start()
+    {
+        EnemiesAlive++;
+    }
+
+    void OnCollisionEnter2D(Collision2D colInfo)
+    {
+        if (colInfo.relativeVelocity.magnitude > health)
+        {
+            Die();
+        }
+    }
+
+    void Die()
+    {
+        Instantiate(deathEffect, transform.position, Quaternion.identity);
+
+        EnemiesAlive--;
+        if (EnemiesAlive <= 0)
+        {
+            menu.SetActive(true);
+            result.text = "You Won";
+        }
+        Destroy(gameObject);
+    }
+
+}
